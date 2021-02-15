fb6a0ba448738fb89d6888c94e99dd2f2c903786
jalan116 <jalan116@stu.southtexascollege.edu>
2021-02-14 18:27:51 -0600

    Scaffold all the things

---
+++ src/components/NewTodo.vue
@@ -0,0 +1,21 @@
+<template>
+    <input
+        class="text-2xl py-2 px-4 rounded-xl w-full mt-8 text-gray-800"
+        type="text"
+        :placeholder="placeholder"
+        v-model="newTodo"
+        @change="addTodo"
+      />
+</template>
+
+<script setuo>
+import {defineProps} from 'vue'
+import {newTodo, addTodo} from '../helpers/useTodos'
+
+define({
+    placeholder: {
+        type: String,
+    default: 'New Todo',
+    },
+})
+</script>
